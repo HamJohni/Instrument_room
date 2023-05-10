16a36d684f66a400dcac55e3473cbf9fab17426f
HamJohni <joni.pacan.wf@gmail.com>
2023-05-10 20:49:33 +0600

    Форма для входа в аккаунт

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Suspense} from "react";
 import NotFound from "./pages/NotFound/NotFound";
 import './style.scss'
 import Register from "./pages/Register/Register";
+import Login from "./pages/Login/Login";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
           <Routes>
               <Route path={'*'} element={<NotFound/>}/>
               <Route path={'/register'} element={<Register/>}/>
+              <Route path={'/login'} element={<Login/>}/>
           </Routes>
       </Suspense>
   );

46837f18bd93557cbeb850735a15107c05f8b68b
HamJohni <joni.pacan.wf@gmail.com>
2023-05-15 20:37:27 +0600

    Закочил главную странницу

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,17 @@
 import Start from "./Start/Start";
+import Catalog from "./Catalog/Catalog";
+import OurBrands from "./OurBrands/OurBrands";
+import AboutUs from "./AboutUs/AboutUs";
+import News from "./News/News";
 
 const Home = () => {
     return(
         <main className={'home'}>
             <Start/>
+            <Catalog/>
+            <OurBrands/>
+            <AboutUs/>
+            <News/>
         </main>
     )
 }
--- src/pages/Layout/Header/switchLang/SwitchLang.jsx
+++ src/pages/Layout/Header/switchLang/SwitchLang.jsx
@@ -1,34 +1,33 @@
-import React, {useEffect, useState} from 'react';
-import {useTranslation} from "react-i18next";
-import {BiWorld} from "react-icons/bi";
-
-const SwitchLang = () => {
-    const [switcher,setSwitcher] = useState(false)
-    const {i18n} = useTranslation()
-    const changesLang = lang => {
-         i18n.changeLanguage(lang)
-    }
-
-    return (
-        <>
-            <li style={{background: switcher ? '#FC573B' : ''}} onClick={() => setSwitcher(prev => !prev)} className="header__cube">
-                <BiWorld/>
-            </li>
-            <div style={{display: switcher ? 'flex' : 'none'}} className="switchLang">
-                <button  onClick={() => {
-                    changesLang('ru')
-                }} className={`switchLang__lang__cont ${i18n.language === 'ru' ? 'active' : ''}`}>
-                    <p className="switch__lang">Russian</p>
-                </button>
-                <button onClick={() => {
-                    changesLang('en')
-                }} className={`switchLang__lang__cont ${i18n.language === 'en' ? 'active' : ''}`}>
-                    <p className="switch__lang">English</p>
-                </button>
-            </div>
-        </>
-
-    );
-};
-
-export default SwitchLang;
+    import React, {useState} from 'react';
+    import {useTranslation} from "react-i18next";
+    import {BiWorld} from "react-icons/bi";
+    
+    const SwitchLang = () => {
+    
+        const [switcher,setSwitcher] = useState(false)
+    
+        const {i18n} = useTranslation()
+    
+        const changesLanguage = (lang) => {
+            i18n.changeLanguage(lang)
+        }
+    
+        return (
+            <>
+                <li style={{background: switcher ? '#FC573B' : ''}} onClick={() => setSwitcher(prev => !prev)} className="header__cube">
+                    <BiWorld/>
+                </li>
+                <div style={{display: switcher ? 'flex' : 'none'}} className="switchLang">
+                    <button  onClick={() => changesLanguage('ru')} className={`switchLang__lang__cont ${i18n.language === 'ru' ? 'active' : ''}`}>
+                        <p className="switch__lang">Russian</p>
+                    </button>
+                    <button onClick={() => changesLanguage('en')} className={`switchLang__lang__cont ${i18n.language === 'en' ? 'active' : ''}`}>
+                        <p className="switch__lang">English</p>
+                    </button>
+                </div>
+            </>
+    
+        );
+    };
+    
+    export default SwitchLang;
